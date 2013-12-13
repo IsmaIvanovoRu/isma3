32b83c3461a106e254b2e8824a75df363a8e5dbe
Vladimir Markovnin <markovnin@gmail.com>
2013-12-13 10:10:31 +0400

    0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Isma::Application.config.secret_key_base = '47c0dfc19a9d311ed57a2547ca3875aca520f369f2ae7c83a9e6d33b8dee8279eeeb52aa9d300adcd8ce62aa71ed88f5c15d5819a835c5494b1c43e3b9a9ba02'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Isma::Application.config.session_store :cookie_store, key: '_isma_session'
