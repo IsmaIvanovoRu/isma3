8833dd19239307292a02ec34a44a1f1556575199
Vladimir Markovnin <markovnin@gmail.com>
2013-12-13 10:56:16 +0400

    Установка devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,8 @@ Isma::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { :host => 'localhost:8081' }
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
