fc3b9a9ea4c28838771a5721dc4e058b31bd910f
Vladimir Markovnin <markovnin@gmail.com>
2013-12-17 10:31:21 +0400

    Шаблонизация вывода списка статей

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   belongs_to :article_type
   has_and_belongs_to_many :attachments
   validate :title, :content, :article_type_id, :presence => true
+  
+  def first_image_attachment
+    attachments.select {|a| a.mime_type =~ /image/}.first
+  end
 end
