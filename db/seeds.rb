8c6e6e1ec5720d0fa734f1127c284886b30dea49
Vladimir Markovnin <markovnin@gmail.com>
2014-01-31 20:27:01 +0400

    Очистка seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #
 #   cities = City.create([{name: ''Chicago' }', '{name: ''Copenhagen' }]')
 #   Mayor.create(name: ''Emanuel'', 'city: 'cities.first')
-Group.where(administrator: true).first.users << User.find(90)
