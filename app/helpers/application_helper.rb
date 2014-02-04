4e59e9ddb8dce4f6d8de2806a8b75f8821d30e4a
Vladimir Markovnin <markovnin@gmail.com>
2014-02-04 16:44:43 +0400

    Заглавная буква в названиях подразделений

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
   def sanitize_full(text)
     Sanitize.clean(text, Sanitize::Config::RELAXED).html_safe
   end
+  
+  def first_letter_upcase(string)
+    string[0] = string[0].mb_chars.upcase
+    return string
+  end
+
 end
