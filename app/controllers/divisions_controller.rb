e28d6adc13c1f9d9395ebd76dc9db43628dd4efe
Vladimir Markovnin <markovnin@gmail.com>
2014-02-16 19:19:18 +0400

    Фикс ошибки отображения совещательных органов

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -46,10 +46,10 @@ class DivisionsController < ApplicationController
 	@articles_fixed = (@division.articles.includes(:attachments).includes(:article_type).order('updated_at DESC').where(published: true, group_id: current_user_groups, fixed: true, division_id: nil).where("exp_date >= ? or exp_date IS ?", Time.now.to_date, nil) + Article.includes(:attachments).includes(:article_type).order('updated_at DESC').where(published: true, group_id: current_user_groups, division_id: @division, fixed: true).where("exp_date >= ? or exp_date IS ?", Time.now.to_date, nil)).uniq
 	@articles_not_fixed = (@division.articles.includes(:attachments).includes(:article_type).order('updated_at DESC').where(published: true, group_id: current_user_groups, fixed: false, division_id: nil).where("exp_date >= ? or exp_date IS ?", Time.now.to_date, nil) + Article.includes(:attachments).includes(:article_type).order('updated_at DESC').where(published: true, group_id: current_user_groups, division_id: @division, fixed: false).where("exp_date >= ? or exp_date IS ?", Time.now.to_date, nil)).uniq.first(5)
       end
-    @clerks = @division.posts.select{|p| p.name =~ /секретарь/}
-    @employees = @division_posts - @head - @clerks
     @childs = Post.where(parent_id: @head).where.not(division_id: @division).map{|p| p.division}.uniq.sort_by(&:name)  
     end
+    @clerks = @division.posts.select{|p| p.name =~ /секретарь/}
+    @employees = @division_posts - @head - @clerks
     @attachment = Attachment.new
     @last_image_attachment = @division.attachments.last
     @menu_title = @division.name if current_user_administrator?
