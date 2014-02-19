83be849999f1dea86fb7b46626293f09d16a579f
Vladimir Markovnin <markovnin@gmail.com>
2014-02-19 23:08:59 +0400

    Замены

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,8 @@ module ApplicationHelper
     return string
   end
 
+  def autosub_details(text)
+    @details_hash.each{|k, v| text.gsub!("&[#{k}]", v.to_s)} if text.include? "&["
+    text
+  end
 end
