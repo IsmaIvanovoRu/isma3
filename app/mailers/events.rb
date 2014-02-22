de45af786c1a2c161717da131c0e506c737c043d
Vladimir Markovnin <markovnin@gmail.com>
2014-02-22 23:41:54 +0400

    Замена адреса присылки сообщения

--- app/mailers/events.rb
+++ app/mailers/events.rb
@@ -4,11 +4,11 @@ class Events < ActionMailer::Base
   
   def new_article(article)
     @article = article
-    mail(to: 'markovnin@isma.ivanovo.ru', subject: 'You have a new article')
+    mail(to: 'webmaster@isma.ivanovo.ru', subject: 'You have a new article')
   end
   
   def new_feedback(feedback)
     @feedback = feedback
-    mail(to: 'markovnin@isma.ivanovo.ru', subject: 'There is a new feedback')
+    mail(to: 'webmaster@isma.ivanovo.ru', subject: 'There is a new feedback')
   end
 end
