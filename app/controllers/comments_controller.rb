362512e0f2ca07984e8c5812a551da85ed6f6d8b
Vladimir Markovnin <markovnin@gmail.com>
2014-03-21 21:34:22 +0400

    Комментарии и обновление гемов

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ArticlesController
-  before_action :require_commentator, only: [:new, :create, :destroy]
-  before_action :require_moderator, only: [:published_toggle]
-  before_action :set_article, only: [:index, :show, :edit, :create, :update, :destroy, :published_toggle]
+  before_filter :require_moderator, only: [:published_toggle]
+  before_filter :require_commentator, only: [:new, :create, :destroy]
+  before_action :set_article
   before_action :set_comment, only: [:show, :edit, :update, :destroy, :published_toggle]
+  before_action :set_comments, only: [:create, :destroy, :published_toggle]
   def index
     @comments = @article.comments.all
   end
@@ -17,7 +18,6 @@ class CommentsController < ArticlesController
   def create
     @comment = @article.comments.new(comment_params)
     @comment.user_id = current_user.id
-    @comments = @article.comments
     respond_to do |format|
       if @comment.save
         format.html { redirect_to :back, notice: 'Comment was successfully created.' }
@@ -37,8 +37,7 @@ class CommentsController < ArticlesController
   end
   
   def destroy
-    @comment.destroy
-    @comments = @article.comments
+    @comment.destroy if current_user_moderator?
     respond_to do |format|
       format.html { redirect_to :back }
       format.js
@@ -47,7 +46,10 @@ class CommentsController < ArticlesController
   
   def published_toggle
     @comment.toggle!(:published)
-    redirect_to :back
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+    end
   end
   
   private
@@ -60,5 +62,9 @@ class CommentsController < ArticlesController
   
   def comment_params
     params.require(:comment).permit(:article_id, :user_id, :content)
+  end
+  
+  def set_comments
+    @comments = current_user_moderator? ? @article.comments.where.not(id: nil) : @article.comments.where.not(id: nil).where(published: true)
   end
 end
