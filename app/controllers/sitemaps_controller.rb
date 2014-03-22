c61bc0e6a4d4f731eebf8e1a070323bf560cae89
Vladimir Markovnin <markovnin@gmail.com>
2014-03-22 17:57:11 +0400

    права на чтение файла sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,4 +1,13 @@
 class SitemapsController < ApplicationController
+  skip_before_filter :authenticate_user!
+  skip_before_filter :current_user
+  skip_before_filter :set_permissions
+  skip_before_filter :set_menus
+  skip_before_filter :profile_empty?
+  skip_before_filter :set_alert
+  skip_before_filter :set_details
+  skip_before_filter :feedbacks_count
+  skip_before_filter :articles_count
   def sitemap
     @articles = Article.select("id, updated_at").where(group_id: nil)
     @divisions = Division.select("id, updated_at")
