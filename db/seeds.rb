c217724fe9042b6cdde98d3626edf9b346a3a10f
Vladimir Markovnin <markovnin@gmail.com>
2014-10-06 13:29:43 +0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,14 @@
 #
 #   cities = City.create([{name: ''Chicago' }', '{name: ''Copenhagen' }]')
 #   Mayor.create(name: ''Emanuel'', 'city: 'cities.first')
-AcademicTitle.create(name: 'старший научный сотрудник')
+extrants = User.joins(:divisions).where("divisions.name like ?", "%6 курса%")
+extrants.each do |user|
+  Group.find(7).users.delete(user)
+end
+Division.where("name like ?", "%6 курса%").each{|d| d.destroy}
+divisions = Division.where(division_type_id: 6)
+divisions.each do |division|
+  name = division.name.split(' ')
+  name[2] = name[2].to_i + 1
+  division.update_attributes(name: name.join(' '))
+end
