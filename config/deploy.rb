58185a666bd7f7fe718f7f00f635be129fee5427
Vladimir Markovnin <markovnin@gmail.com>
2015-05-22 14:41:01 +0300

    Changed restart command

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,8 +43,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      set :password, 'ubuntu'
-      execute "sudo service thin restart"
+      execute "thin restart -C /etc/thin/isma.yml"
     end
   end
 
