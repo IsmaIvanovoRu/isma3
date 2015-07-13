dad8f5e7ad2860c945fe49278625063d2b4992c3
Vladimir Markovnin <markovnin@gmail.com>
2015-07-13 10:24:31 +0300

    Removed attachment include from archive controller

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -3,14 +3,14 @@ class ArchivesController < ApplicationController
   before_action :article_params
   def index
     if current_user.nil?
-      @articles = Article.includes(:attachments).order('updated_at DESC').where(published: true, group_id: nil).where(article_params).paginate(:page => params[:page])
+      @articles = Article.order('updated_at DESC').where(published: true, group_id: nil).where(article_params).paginate(:page => params[:page])
     else
       if current_user_moderator?
 	current_user_groups = Group.all + [nil]
-	@articles = Article.includes(:attachments).order('updated_at DESC').where(group_id: current_user_groups).where(article_params).paginate(:page => params[:page])
+	@articles = Article.order('updated_at DESC').where(group_id: current_user_groups).where(article_params).paginate(:page => params[:page])
       else
 	current_user_groups = current_user.groups + current_user.groups.joins(:parent).map{|g| g.parent} + [nil]
-	@articles = Article.includes(:attachments).order('updated_at DESC').where(group_id: current_user_groups, published: true).where(article_params).paginate(:page => params[:page])
+	@articles = Article.order('updated_at DESC').where(group_id: current_user_groups, published: true).where(article_params).paginate(:page => params[:page])
       end
     end
   end
