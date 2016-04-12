21a8cb972dc7e54b48ccb04f442748633e7bca1d
Vladimir Markovnin <markovnin@gmail.com>
2016-04-12 16:12:27 +0300

    Changed Abitur path

--- app/controllers/abitur_controller.rb
+++ app/controllers/abitur_controller.rb
@@ -2,6 +2,6 @@ class AbiturController < ApplicationController
   skip_before_filter :authenticate_user!
   
   def index
-    redirect_to division_path(88)
+    redirect_to article_path(8)
   end
 end
