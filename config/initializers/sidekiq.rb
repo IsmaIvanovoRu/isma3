90b06f7e8f105b7051a598109c0c7ff778ca2baa
Vladimir Markovnin <markovnin@gmail.com>
2016-10-09 19:42:13 +0300

    fix redis address

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://10.0.3.162:6379/12' }
+  config.redis = { url: 'redis://10.0.5.184:6379/12' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://10.0.3.162:6379/12' }
+  config.redis = { url: 'redis://10.0.5.184:6379/12' }
 end
