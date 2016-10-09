290deb1c392895ed938191f659b5414150f10d6b
Vladimir Markovnin <markovnin@gmail.com>
2016-10-09 18:19:22 +0300

    fix server name in cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ role :db,  %w{isma}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'isma_temp', user: 'ubuntu', roles: %w{web app}
+server 'isma', user: 'ubuntu', roles: %w{web app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
