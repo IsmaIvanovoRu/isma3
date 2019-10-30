7cc273c86c757b31321020878cdc96438fd76d4c
Ubuntu <markovnin@isma.ivanovo.ru>
2019-10-30 07:01:02 +0000

    add dependent destroy to classrooms

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,7 +2,7 @@ class Subject < ActiveRecord::Base
   belongs_to :attachment
   belongs_to :educational_program
   has_and_belongs_to_many :posts 
-  has_many :classrooms
+  has_many :classrooms, dependent: :destroy
   
   validates :name, :educational_program_id, presence: true
   validates :educational_program_id, numericality: { integer_only: true }
