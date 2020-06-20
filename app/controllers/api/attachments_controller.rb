2a7b8e4cd93454766b75075d9f1006a5ed33842e
Vladimir Mrakovnin <markovnin@isma.ivanovo.ru>
2020-06-20 09:32:29 +0000

    try fix http ssl schema

--- app/controllers/api/attachments_controller.rb
+++ app/controllers/api/attachments_controller.rb
@@ -4,6 +4,7 @@ class Api::AttachmentsController < ApplicationController
     method = 'attachments/'
     http_params = http_params()
     http = Net::HTTP.new(http_params[:uri_host], http_params[:uri_port], http_params[:proxy_ip], http_params[:proxy_port])
+    http.use_ssl = true if Rails.env == 'production'
     response = http.get(http_params[:uri_path] + method + params[:id])
     send_data response.body, filename: params[:filename], disposition: 'attachment'
   end
