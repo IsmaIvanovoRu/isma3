49cb9fec65414b79ef365503c075aa2237f55d0a
Vladimir Mrakovnin <markovnin@isma.ivanovo.ru>
2020-06-20 08:18:28 +0000

    fix http ssl schema

--- app/controllers/entrants_controller.rb
+++ app/controllers/entrants_controller.rb
@@ -6,7 +6,8 @@ class EntrantsController < ApplicationController
   def show
     method = 'entrant_applications/'
     http_params = http_params()
-    http = Net::HTTP.new(http_params[:uri_host], http_params[:uri_port], http_params[:proxy_ip], http_params[:proxy_port])
+    http = Net::HTTP.new(http_params[:uri_host], http_params[:uri_port], http_params[:proxy_ip], http_params[:proxy_port]) if Rails.env == 'development'
+    http = Net::HTTP.new(http_params[:uri_host], http_params[:uri_port], http_params[:proxy_ip], http_params[:proxy_port], :use_ssl => uri.scheme == 'https') if Rails.env == 'production'
     response = http.get(http_params[:uri_path] + method + params[:id])
     @entrant_application = JSON.parse(response.body)
   end
