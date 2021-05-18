5489157c8e6e1f3cb2ed8a4ef4b4673d3762c0e5
Vladimir Mrakovnin <markovnin@isma.ivanovo.ru>
2021-05-18 09:38:18 +0000

    sveden financial activity

--- app/models/financial_activity.rb
+++ app/models/financial_activity.rb
@@ -1,5 +1,5 @@
 class FinancialActivity < ActiveRecord::Base
-  validates :year, :federal_volume, :regional_volume, :municipal_volume, :personal_volume, :financial_report_link, :financial_plan_link, :bus_gov_link, presence: true
+  validates :year, presence: true
   validates :year, numericality: { integer_only: true }
   validates :year, uniqueness: true
   validates :federal_volume, :regional_volume, :municipal_volume, :personal_volume, numericality: true
