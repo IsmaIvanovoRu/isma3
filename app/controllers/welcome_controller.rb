ec49f6f59001ae1fe13d3a358a17f462d3b3b56f
Vladimir Mrakovnin <markovnin@isma.ivanovo.ru>
2021-06-25 19:11:05 +0000

    accreditation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   skip_before_filter :authenticate_user!
   def index
-    redirect_to articles_path if user_signed_in?
+    redirect_to user_profile_path(current_user) if user_signed_in?
+    #redirect_to articles_path if user_signed_in?
   end
 end
